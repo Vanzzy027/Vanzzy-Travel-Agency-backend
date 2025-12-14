1114feb9aa0dc6694ddfce7147aa6b0ab0005a8f
Evans Kariuki <118306655+Vanzzy027@users.noreply.github.com>
2025-12-14 23:05:34 +0300

    Delete dist directory

--- dist/auth/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { createUser, loginUser, forgotPassword, resetPassword } from "./auth.controller.js";
-const authRouter = new Hono();
-// Define the routes
-authRouter.post("/register", createUser);
-authRouter.post("/login", loginUser);
-authRouter.post("/forgot-password", forgotPassword);
-authRouter.post("/reset-password", resetPassword);
-export default authRouter;
